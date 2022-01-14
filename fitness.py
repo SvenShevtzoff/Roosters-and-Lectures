c682bdca2d3731c53f12eee7166b26bd5033f652
Vera <duindamvera@gmail.com>
2022-01-14 12:58:50 +0100

    Fitness function

--- fitness.py
+++ fitness.py
@@ -28,62 +28,37 @@ def dict_to_df(roomslots):
 
 
 def fitness_function(schedule):
-    # print(schedule)
     malus_points = 0
     # aantal studenten groter dan maximum toegestaan in de zaal (1)
-    # for i in schedule.index:
-    #     if schedule["activity"][i]:
-    #         max_students_in_activity = schedule["activity"][i].get_max_stud()
-    #         room_capacity = schedule["room"][i].get_capacity()
-    #         difference = max_students_in_activity - room_capacity
-    #         if difference > 0:
-    #             malus_points += difference
+    for i in schedule.index:
+        if schedule["activity"][i]:
+            number_of_students = len(schedule["students"][i])
+            room_capacity = schedule["room"][i].get_capacity()
+            difference = number_of_students - room_capacity
+            if difference > 0:
+                malus_points += difference
 
-    # print(malus_points)
-
-    # # gebruik van avondslot (5)
-    # schedule_evening = schedule.loc[(schedule["time"] == 17) & schedule["activity"]]
-    # malus_points += 5 * len(schedule_evening.index)
-
-    # print(malus_points)
+    # gebruik van avondslot (5)
+    schedule_evening = schedule.loc[(schedule["time"] == 17) & schedule["activity"]]
+    malus_points += 5 * len(schedule_evening.index)
 
     # # vakconflicten (1)
     schedule_exploded = schedule.explode("students")
-
     schedule_conflicts = schedule_exploded[["students", "day", "time", "activity"]].groupby(["students", "day", "time"]).count()
 
-    # for i in schedule_conflicts.index:
-    #     if schedule_conflicts["activity"][i] > 1:
-    #         malus_points += (schedule_conflicts["activity"][i] - 1)
-    # schedule_conflicts.to_csv("students.csv")
-    # print(malus_points)
+    for i in schedule_conflicts.index:
+        if schedule_conflicts["activity"][i] > 1:
+            malus_points += (schedule_conflicts["activity"][i] - 1)
+    schedule_conflicts.to_csv("students.csv")
 
-    # # één tussenslot (1)
-    # previous_student = ("dummy_name", "dummy_day", "dummy_time")
-    # for row in schedule_conflicts.index:
-    #     if row[0:2] == previous_student[0:2]:
-    #         if abs(row[2] - previous_student[2]) == 4:
-    #             malus_points += 1
-    #         if abs(row[2] - previous_student[2]) == 6:
-    #             malus_points += 3
-    #     previous_student = row
-
-    # print(schedule_conflicts.head(10))
-    # print(malus_points)
-
-    # tryout
-    tryout_df = schedule_conflicts.head(10)
-    print(tryout_df)
-
+    # één tussenslot (1) of twee tussensloten (3)
     previous_student = ("dummy_name", "dummy_day", "dummy_time")
-    for row in tryout_df.index:
+    for row in schedule_conflicts.index:
         if row[0:2] == previous_student[0:2]:
             if abs(row[2] - previous_student[2]) == 4:
                 malus_points += 1
             if abs(row[2] - previous_student[2]) == 6:
                 malus_points += 3
-        print(malus_points)
         previous_student = row
-    # twee tussensloten (3)
 
     return malus_points
