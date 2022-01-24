7a0e290dde4140a9bcf0a5560b6d99e06359d09a
SvenShevtzoff <sshevtzoff@gmail.com>
2022-01-24 14:45:38 +0100

    Fix bug where lectures had a maximum

--- code/algorithms/hillclimber.py
+++ code/algorithms/hillclimber.py
@@ -2,62 +2,52 @@
 # hillclimber.py with random algoritm functions
 # =============================================================================
 from code.algorithms.randomise import randomise
-from code.algorithms.helpers import swap_activities, move_students
 import random
 
-def hill_climber_alg(schedule, iterations, count_until_startover=100, mutations=5):
+def swap_activities(roomslot1, roomslot2):
+    activity1 = roomslot1.activity()
+    activity2 = roomslot2.activity()
+    if activity1 and activity2:
+        helper = roomslot1
+        activity1.set_roomslot(roomslot2)
+        activity2.set_roomslot(helper)
+    elif not activity1 and activity2:
+        activity2.set_roomslot(roomslot1)
+        roomslot2.remove_activity()
+    elif activity1 and not activity2:
+        activity1.set_roomslot(roomslot2)
+        roomslot1.remove_activity()
+    else:
+        pass
+
+
+def move_students(student, from_activity, to_activity):
+    """Moves a student from one activity to another"""
+    from_activity.remove_student(student)
+    student.remove_activity(from_activity)
+    to_activity.add_student(student)
+    student.add_activity(to_activity)
+
+def mutate(schedule):
+    roomslot1 = random.choice(schedule.roomslots().list())
+    roomslot2 = random.choice(schedule.roomslots().list())
+
+    swap_activities(roomslot1, roomslot2)
+
+def hill_climber_alg(schedule, iterations):
     current_schedule = randomise(schedule)
-    old_schedule = current_schedule.copy()
-    i = 0 
-    while i != count_until_startover:
 
-        #     Doe een kleine random aanpassing
-        for _ in range(mutations):
-            roomslot1 = random.choice(current_schedule.roomslots().list())
-            roomslot2 = random.choice(current_schedule.roomslots().list())
+    for iteration in range(iterations):
+        # maak een kopie
+        new_schedule = current_schedule.copy()
 
-            swap_activities(roomslot1, roomslot2)
+        # maak een mutatie
+        mutate(new_schedule)
 
-        if current_schedule.fitness() >= old_schedule.fitness():
-            current_schedule = old_schedule.copy()
-            print("slechter")
-            i += 1
-        else:
-            old_schedule = current_schedule.copy()
-            print("beter")
-            i = 0
-  
-        print(current_schedule.fitness())
+        # verbeterd
+        if new_schedule.fitness() <= current_schedule.fitness():
+            # print("if")
+            print(new_schedule.fitness())
+            current_schedule = new_schedule
 
-    
-        
-
-
-    
-
-
-
-# <<<<<<< HEAD
-
-    
-
-
-
-# import copy
-# import random
-
-# from .randomise import randomise
-
-# class HillClimber:
-
-#     def __init__(self, schedule):
-#         pass
-
-#     def run():
-#         pass
-        # Kies een random start state
-        # Herhaal:
-        
-       
-        #         Maak de aanpassing ongedaan
-#         swap_activities(roomslot1, roomslot2)
+    return current_schedule
